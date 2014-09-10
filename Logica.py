1bfe5821f05f5de7be03b498c44043f7e68aac0b
dsvargas <dsvargas.211@gmail.com>
2014-09-11 00:43:41 +0600

    menu y logica
    menu de inicio y guarda las lineas del programa

---
+++ Logica.py
@@ -0,0 +1,24 @@
+__author__ = 'Dylana'
+#Nombre: Dylana Sancho
+#Fecha de entrega: 17-09-2014
+#horas de trabajo:
+
+
+tipovariable = [{"int":"num"},{"float":"deci"},{"str":"letter"},{"True":"V"},{"False":"F"}]
+
+Condicionales = [{"if":"si"},{"elif":"sifi"},{"else":"sino"}]
+
+Bucles = [{"while":"mientras"},{"for":"hagase"}]
+
+Funciones = [{"print":"imprimir"},{"def":"funka"},{"return":"retorne"},{"":""}]
+
+sintaxis = [{":":";"},{"[":"("},{"]":")"},{"(":"{"},{")":"}"},{" ":"_"},{"    ":"//"}]
+
+
+def reading(lineas):
+    while True:
+        line = input()
+        if line == "listo":
+            return
+        lineas.append(line)
+
---
+++ Main.py
@@ -0,0 +1,32 @@
+__author__ = 'Dylana'
+# Nombre: Dylana Sancho
+#Fecha de entrega: 17-09-2014
+#horas de trabajo:
+
+from Logica import *
+
+lineas = []
+
+
+def main():
+    print("1.Nuevo archivo")
+    print("2.Compilar")
+    try:
+        qs = int(input("Escoja la opcion que desee."))
+        if qs == 1:
+            print("Nuevo archivo")
+            reading(lineas)
+        elif qs == 2:
+            print("Compilar")
+        else:
+            print("cosita")
+            main()
+    except:
+        print("cosita")
+        main()
+
+main()
+
+def revisa(lineas):
+    for i in lineas:
+        lineas.split(" ")
